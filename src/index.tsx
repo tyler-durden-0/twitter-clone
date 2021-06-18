ff958ed7580ca9d530c830280ee267a1bbd9cab4
Ivan <ivanchernetskey@mail.ru>
2021-06-18 20:09:45 +0300

    write all what i can see in video about styles on this time

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { ThemeProvider } from '@material-ui/core/styles'
 
 import './index.css';
 
+import App from './App';
 import {theme} from './theme'
 
 ReactDOM.render(
