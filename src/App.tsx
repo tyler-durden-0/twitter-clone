43311391626d201f10a3c4765f3b41d20649e58f
Ivan <ivanchernetskey@mail.ru>
2021-06-18 19:26:38 +0300

    now we import in the right way

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import SignIn from './pages/SignIn'
-import { ThemeProvider } from '@material-ui/styles';
-import {theme} from './theme'
-
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
       <SignIn />
-    </ThemeProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ThemeProvider } from '@material-ui/core/styles'
 
 import './index.css';
 
+import {theme} from './theme'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ThemeProvider theme={theme}>
+          <App />
+      </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
