7368f7679ccdf7f34aef49fbaab48783caf10517
Ivan <ivanchernetskey@mail.ru>
2021-06-28 14:51:14 +0300

    make Tweet component

---
+++ src/components/Tweet/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import classNames from "classnames";
+import {Avatar, Grid, Paper, Typography} from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import CommentIcon from "@material-ui/icons/ChatBubbleOutline";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import LikeIcon from "@material-ui/icons/FavoriteBorderOutlined";
+import ShareIcon from "@material-ui/icons/ReplyOutlined";
+import {useHomeStyles} from "../../pages/Home";
+
+
+interface TweetProps {
+    text: string
+    classes: ReturnType<typeof useHomeStyles>
+    user: {
+        fullName: string
+        userName: string
+        avatarUrl: string
+    }
+}
+
+export const Tweet: React.FC<TweetProps> = ({text, user, classes}: TweetProps): React.ReactElement => {
+    return (
+        <Paper className={classNames(classes.tweet, classes.tweetsHeader)} variant='outlined'>
+            <Grid container spacing={3}>
+                <Grid item xs={1}>
+                    <Avatar
+                        className={classes.tweetAvatar}
+                        alt={`Аватарка пользователя ${user.fullName}`}
+                        src={user.avatarUrl}
+                    />
+                </Grid>
+                <Grid item xs={11}>
+                    <Typography>
+                        <b>{user.fullName}</b>
+                        <span className={classes.tweetUserName}>@{user.userName}</span>&nbsp;
+                        <span className={classes.tweetUserName}>.</span>&nbsp;
+                        <span className={classes.tweetUserName}>1 час</span>
+                    </Typography>
+                    <Typography variant='body1' gutterBottom>
+                        {text}
+                    </Typography>
+                    <div className={classes.tweetFooter}>
+                        <div>
+                            <IconButton color="primary">
+                                <CommentIcon style={{fontSize: 20}} />
+                            </IconButton>
+                            <span>1</span>
+                        </div>
+                        <div>
+                            <IconButton color="primary">
+                                <RepeatIcon style={{fontSize: 20}} />
+                            </IconButton>
+                        </div>
+                        <div>
+                            <IconButton color="primary">
+                                <LikeIcon style={{fontSize: 20}} />
+                            </IconButton>
+                        </div>
+                        <div>
+                            <IconButton color="primary">
+                                <ShareIcon style={{fontSize: 20}} />
+                            </IconButton>
+                        </div>
+                    </div>
+                </Grid>
+            </Grid>
+        </Paper>
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import {
-    Avatar,
     Container,
     createStyles,
     Grid,
     InputBase,
     makeStyles,
     Paper,
+    Theme,
     Typography,
     withStyles
 } from "@material-ui/core";
-import classNames from 'classnames'
 import TwitterIcon from "@material-ui/icons/Twitter";
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
@@ -20,12 +19,9 @@ import BookmarkBorderOutlinedIcon from '@material-ui/icons/BookmarkBorderOutline
 import ListAltOutlinedIcon from '@material-ui/icons/ListAltOutlined';
 import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
 import grey from '@material-ui/core/colors/grey'
-import CommentIcon from '@material-ui/icons/ChatBubbleOutline';
-import RepeatIcon from '@material-ui/icons/Repeat';
-import LikeIcon from '@material-ui/icons/FavoriteBorderOutlined';
-import ShareIcon from '@material-ui/icons/ReplyOutlined';
+import {Tweet} from "../components/Tweet";
 
-const useHoeStyles = makeStyles(() => ({
+export const useHomeStyles = makeStyles((theme: Theme) => ({
     wrapper: {
         height: '100vh'
     },
@@ -71,12 +67,20 @@ const useHoeStyles = makeStyles(() => ({
     },
     tweet: {
         cursor: 'pointer',
+        paddingTop: 15,
+        paddingLeft: 20,
         '&:hover': {
             backgroundColor: 'rgb(245, 248, 250)'
         }
     },
+    tweetAvatar: {
+        width: theme.spacing(5),
+        height: theme.spacing(5)
+    },
     tweetFooter: {
         display: 'flex',
+        position: 'relative',
+        left: -13,
         justifyContent: 'space-between',
         width: 450
     },
@@ -97,7 +101,7 @@ const CssTextField = withStyles(() =>
 )(InputBase);
 
 export const Home = () => {
-    const classes = useHoeStyles()
+    const classes = useHomeStyles()
 
     return (
         <Container className={classes.wrapper} maxWidth="lg">
@@ -152,48 +156,14 @@ export const Home = () => {
                         <Paper className={classes.tweetsHeader} variant='outlined'>
                             <Typography variant='h6'>Главная</Typography>
                         </Paper>
-                        <Paper className={classNames(classes.tweet, classes.tweetsHeader)} variant='outlined'>
-                            <Grid container spacing={3}>
-                                <Grid item xs={1}>
-                                    <Avatar
-                                        alt="User Avatar"
-                                        src="https://images.unsplash.com/photo-1533227268428-f9ed0900fb3b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=579&q=80"
-                                         />
-                                </Grid>
-                                <Grid item xs={11}>
-                                    <Typography><b>mxhxvoid</b> <span className={classes.tweetUserName}>@mahavoid</span></Typography>
-                                    <Typography variant='body1' gutterBottom>
-                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                                    </Typography>
-                                    <div className={classes.tweetFooter}>
-                                        <div>
-                                            <IconButton>
-                                                <CommentIcon style={{fontSize: 20}} />
-                                            </IconButton>
-                                            <span>1</span>
-                                        </div>
-                                        <div>
-                                            <IconButton>
-                                                <RepeatIcon style={{fontSize: 20}} />
-                                            </IconButton>
-                                        </div>
-                                        <div>
-                                            <IconButton>
-                                                <LikeIcon style={{fontSize: 20}} />
-                                            </IconButton>
-                                        </div>
-                                        <div>
-                                            <IconButton>
-                                                <ShareIcon style={{fontSize: 20}} />
-                                            </IconButton>
-                                        </div>
-                                    </div>
-
-                                </Grid>
-                            </Grid>
-                        </Paper>
+                        <Tweet
+                            user={{
+                                fullName:'Иван',
+                                userName: 'Чернецкий Иван Романович',
+                                avatarUrl: 'https://sun9-71.userapi.com/impf/c851416/v851416586/1e8a6b/86K3drulzUE.jpg?size=366x393&quality=96&sign=43981b77b080e0c9c21edff8a762058f&type=album'
+                            }}
+                            classes={classes}
+                            text='It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' />
                     </Paper>
                 </Grid>
                 <Grid item xs={3}>
