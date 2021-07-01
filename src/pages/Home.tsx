c834ef62ca3a31995c7e021d6c6bce80062a7de4
Ivan <ivanchernetskey@mail.ru>
2021-07-01 16:25:52 +0300

    write styles from 3 lesson before coding

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -103,6 +103,7 @@ export const useHomeStyles = makeStyles((theme: Theme) => ({
         }
     },
     tweet: {
+        display: 'flex',
         cursor: 'pointer',
         paddingTop: 15,
         paddingLeft: 20,
@@ -111,8 +112,9 @@ export const useHomeStyles = makeStyles((theme: Theme) => ({
         }
     },
     tweetAvatar: {
-        width: theme.spacing(5),
-        height: theme.spacing(5)
+        width: theme.spacing(6.5),
+        height: theme.spacing(6.5),
+        marginRight: 15
     },
     tweetFooter: {
         display: 'flex',
@@ -123,6 +125,86 @@ export const useHomeStyles = makeStyles((theme: Theme) => ({
     },
     tweetUserName: {
         color: grey[500]
+    },
+    rightSide: {
+        paddingTop: 20,
+        position: 'sticky',
+        top: 0
+    },
+    rightSideBlock: {
+        backgroundColor: '#F5F8FA',
+        borderRadius: 15,
+        marginTop: 15,
+        '& .MuiList-root': {
+            paddingTop: 0
+        }
+    },
+    rightSideBlockHeader: {
+        borderTop: 0,
+        borderLeft: 0,
+        borderRight: 0,
+        backgroundColor: 'transparent',
+        padding: '13px 18px',
+        '& b': {
+            fontSize: 20,
+            fontWeight: 800
+        }
+    },
+    rightSideBlockItem: {
+        cursor: 'pointer',
+        '& .MuiTypography-body1': {
+            fontWeight: 700
+        },
+        '& .MuiListItemAvatar-root': {
+            minWidth: 50
+        },
+        '& .MuiListItemText-root': {
+            margin: 0
+        },
+        '&:hover': {
+            backgroundColor: '#edf3f6'
+        }
+    },
+    addForm: {
+        padding: 20
+    },
+    addFormBody: {
+        display: 'flex',
+        width: '100%'
+    },
+    addFormBottom: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    addFormBottomActions: {
+        marginTop: 10,
+        paddingLeft: 70
+    },
+    addFormTextarea: {
+        width: '100%',
+        border: 0,
+        fontSize: 20,
+        outline: 'none',
+        fontFamily: 'inherit',
+        resize: 'none'
+    },
+    addFormBottomLine: {
+        height: 12,
+        backgroundColor: '#E6ECF0'
+    },
+    addFormCircleProgress: {
+        position: 'relative',
+        width: 20,
+        height: 20,
+        margin: '0 10px',
+        '& .MuiCircleProgress-root': {
+            position: 'absolute'
+        }
+    },
+    addFormBottomRight: {
+        display: 'flex',
+        alignItems: 'center'
     }
 }))
 
